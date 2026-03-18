d8b0627b1d939c6fc2c5006995ea2c0bc2ccae4a
Ujjwal Kandpal <153886946+mrujjwalkandpal@users.noreply.github.com>
2026-03-18 20:20:16 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,30 +1,23 @@
 export const foodData = [
-      {
-          id: 1,
-              name: "Burger",
-                  vendor: "Food Plaza",
-                      price: 120,
-                          image: "https://source.unsplash.com/200x200/?burger"
-                            },
-                              {
-                                  id: 2,
-                                      name: "Pizza",
-                                          vendor: "Domino's",
-                                              price: 250,
-                                                  image: "https://source.unsplash.com/200x200/?pizza"
-                                                    },
-                                                      {
-                                                          id: 3,
-                                                              name: "Cold Coffee",
-                                                                  vendor: "Cafe Coffee Day",
-                                                                      price: 80,
-                                                                          image: "https://source.unsplash.com/200x200/?coffee"
-                                                                            },
-                                                                              {
-                                                                                  id: 4,
-                                                                                      name: "Sandwich",
-                                                                                          vendor: "Subway",
-                                                                                              price: 150,
-                                                                                                  image: "https://source.unsplash.com/200x200/?sandwich"
-                                                                                                    }
-                                                                                                    ];
+  {
+    id: 1,
+    name: "Burger",
+    vendor: "Food Plaza",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd"
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    vendor: "Domino's",
+    price: 250,
+    image: "https://images.unsplash.com/photo-1601924582975-7e6f3c0e8d8b"
+  },
+  {
+    id: 3,
+    name: "Cold Coffee",
+    vendor: "Cafe",
+    price: 80,
+    image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+  }
+];
