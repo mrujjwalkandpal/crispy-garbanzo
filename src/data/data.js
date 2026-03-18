461d8b6348b35a58a1a73e579700b49daf61028c
Ujjwal Kandpal <153886946+mrujjwalkandpal@users.noreply.github.com>
2026-03-18 20:43:26 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,72 +1,72 @@
 export const foodData = [
   {
     id: 1,
-    name: "Burger",
-    vendor: "Food Plaza",
-    price: 120,
-    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=200&q=40"
-  },
-  {
-    id: 2,
-    name: "Pizza",
-    vendor: "Domino's",
-    price: 250,
-    image: "https://images.unsplash.com/photo-1601924582975-7e6f3c0e8d8b?w=200&q=40"
-  },
-  {
-    id: 3,
     name: "Cold Coffee",
-    vendor: "Cafe",
+    vendor: "Nescafe",
     price: 80,
     image: "https://images.unsplash.com/photo-1509042239860-f550ce710b93?w=200&q=40"
   },
   {
-    id: 4,
-    name: "Sandwich",
-    vendor: "Subway",
-    price: 150,
+    id: 2,
+    name: "Cappuccino",
+    vendor: "Nescafe",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?w=200&q=40"
+  },
+  {
+    id: 3,
+    name: "Veg Sandwich",
+    vendor: "Nescafe",
+    price: 70,
     image: "https://images.unsplash.com/photo-1553909489-cd47e0ef937f?w=200&q=40"
   },
   {
+    id: 4,
+    name: "Rajma Chawal",
+    vendor: "Main Canteen",
+    price: 100,
+    image: "https://images.unsplash.com/photo-1604908177522-4321bfa2b9d2?w=200&q=40"
+  },
+  {
     id: 5,
-    name: "Pasta",
-    vendor: "Italian Hub",
-    price: 200,
-    image: "https://images.unsplash.com/photo-1589308078059-be1415eab4c3?w=200&q=40"
+    name: "Chole Bhature",
+    vendor: "Main Canteen",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1626132647523-66f1c1d9b1c1?w=200&q=40"
   },
   {
     id: 6,
-    name: "French Fries",
-    vendor: "Snack Corner",
-    price: 90,
-    image: "https://images.unsplash.com/photo-1573080496219-bb080dd4f877?w=200&q=40"
+    name: "Thali",
+    vendor: "Main Canteen",
+    price: 150,
+    image: "https://images.unsplash.com/photo-1617196038435-1e59b1a1a6c7?w=200&q=40"
   },
   {
     id: 7,
-    name: "Momos",
-    vendor: "Street Bites",
-    price: 110,
-    image: "https://images.unsplash.com/photo-1604908176997-4312c6fdfd41?w=200&q=40"
+    name: "Maggi",
+    vendor: "Tuck Shop",
+    price: 50,
+    image: "https://images.unsplash.com/photo-1604908811925-0b73d8d7b2c6?w=200&q=40"
   },
   {
     id: 8,
-    name: "Ice Cream",
-    vendor: "Creamy Delights",
-    price: 70,
-    image: "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?w=200&q=40"
+    name: "Samosa",
+    vendor: "Tuck Shop",
+    price: 20,
+    image: "https://images.unsplash.com/photo-1601050690597-df0568f70950?w=200&q=40"
   },
   {
     id: 9,
-    name: "Noodles",
-    vendor: "Wok Express",
-    price: 140,
-    image: "https://images.unsplash.com/photo-1585032226651-759b368d7246?w=200&q=40"
+    name: "Chips",
+    vendor: "Tuck Shop",
+    price: 30,
+    image: "https://images.unsplash.com/photo-1585238342028-4d0c1c1c1c1c?w=200&q=40"
   },
   {
     id: 10,
-    name: "Paneer Roll",
-    vendor: "Roll Center",
-    price: 130,
-    image: "https://images.unsplash.com/photo-1601050690597-df0568f70950?w=200&q=40"
+    name: "Cold Drink",
+    vendor: "Tuck Shop",
+    price: 40,
+    image: "https://images.unsplash.com/photo-1580910051074-3eb694886505?w=200&q=40"
   }
 ];
